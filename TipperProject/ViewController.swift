df2b0b4a440eb4d3eae3514a26a649c47c33e37d
Kennedi Cosma <kencosma2020@outlook.com>
2020-09-06 12:30:18 -0400

    Initial Commit

---
+++ TipperProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipperProject
+//
+//  Created by Kennedi Cosma on 9/6/20.
+//  Copyright © 2020 Kennedi Cosma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
